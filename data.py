eebb6fee0442eff4a9a8b4218254f35b038cef5d
Frida Hernandez <if720476@iteso.mx>
2022-09-01 14:02:14 -0500

    Agregar archivos base

---
+++ data.py
@@ -0,0 +1,17 @@
+
+"""
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
+# -- script: data.py : python script for data collection                                                 -- #
+# -- author: YOUR GITHUB USER NAME                                                                       -- #
+# -- license: GPL-3.0 License                                                                            -- #
+# -- repository: YOUR REPOSITORY URL                                                                     -- #
+
+Created on Wed Aug 24 14:35:56 2022
+
+@author: FridaHernandezL
+"""
+import pandas as pd
+import numpy as np 
+
+data=pd.read_csv('NAFTRAC_20200131.csv')
